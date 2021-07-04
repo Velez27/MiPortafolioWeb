5eddeab8ca420ccf8b31820ea78755afb92655b6
Jesus Velez <jvf_velez@hotmail.com>
2021-07-04 00:37:06 -0500

    Se agregan las imagenes y enlaces de la lista de proyectos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,7 +9,10 @@ import Footer from '../components/Footer';
 import CarruselItemImage from '../components/CarruselItemImage';
 
 //Imagenes principales para los proyectos
-import proyectImage from '../assets/static/proyecto-rickandmorty.png'
+import proyectImage_1 from '../assets/static/proyecto-portafoliov2.png';
+import proyectImage_2 from '../assets/static/proyecto-rickandmorty.png';
+import proyectImage_3 from '../assets/static/proyecto-paginaweb.png';
+import proyectImage_4 from '../assets/static/proyecto-sistemareportes.png';
 
 //Imagenes y ALT para las tecnologias de los proyectos
 import jsIcon from '../assets/static/javascript-icon.png';
@@ -18,6 +21,12 @@ import htmlIcon from '../assets/static/html-icon.png';
 const altIconHTML = 'Icono HTML';
 import cssIcon from '../assets/static/css-icon.png'
 const altIconCSS = 'Icono CSS';
+import reactjs from '../assets/static/reactjs-icon.png';
+const altReactJS = 'Icono React JS';
+import sass from '../assets/static/scss-icon.png';
+const altSass = 'Icono SCSS';
+import javaIcon from '../assets/static/java-icon.png';
+const altJava = 'Icono Java';
 
 const App = () => (
   <div className='App'>
@@ -25,22 +34,25 @@ const App = () => (
     <AcercaDe/>
     <ListaProyectos title='Mis Proyectos'>
       <Carrusel>
-        <CarruselItem projectImage={proyectImage} linkGitProject="https://github.com/Velez27/MiPortafolioWeb" title="Proyecto Personal" description="Consumiendo una API">
+      <CarruselItem projectImage={proyectImage_1} linkGitProject="https://github.com/Velez27/MiPortafolioWeb" title="Proyecto Personal" description="Desarrollo de mi Portafolio WEB v2.0">
+          <CarruselItemImage icono={reactjs} alt={altReactJS} />
+          <CarruselItemImage icono={sass} alt={altSass} />
+          <CarruselItemImage icono={htmlIcon} alt={altIconHTML} />
+          <CarruselItemImage icono={jsIcon} alt={altIconJS} />
+          <CarruselItemImage icono={cssIcon} alt={altIconJS} />
+        </CarruselItem>
+        <CarruselItem projectImage={proyectImage_2} linkGitProject="https://github.com/Velez27/personajesRickAndMorty" title="Proyecto Personal" description="Consumiendo una API con JavaScript">
           <CarruselItemImage icono={jsIcon} alt={altIconJS} />
           <CarruselItemImage icono={htmlIcon} alt={altIconHTML} />
           <CarruselItemImage icono={cssIcon} alt={altIconCSS} />
         </CarruselItem>
-        <CarruselItem projectImage={proyectImage} title="Proyecto Privado" description="Pagina Web">
+        <CarruselItem projectImage={proyectImage_3} linkGitProject="https://github.com/Velez27/Portafolio" title="Proyecto Personal" description="Desarrollo de mi Porfatolio WEB v1.0">
+          <CarruselItemImage icono={htmlIcon} alt={altIconHTML} />
           <CarruselItemImage icono={jsIcon} alt={altIconJS} />
           <CarruselItemImage icono={cssIcon} alt={altIconJS} />
         </CarruselItem>
-        <CarruselItem projectImage={proyectImage} title="Proyecto Privado" description="Pagina Web">
-          <CarruselItemImage icono={jsIcon} alt={altIconJS} />
-          <CarruselItemImage icono={cssIcon} alt={altIconJS} />
-        </CarruselItem>
-        <CarruselItem projectImage={proyectImage} title="Proyecto Privado" description="Pagina Web">
-          <CarruselItemImage icono={jsIcon} alt={altIconJS} />
-          <CarruselItemImage icono={cssIcon} alt={altIconJS} />
+        <CarruselItem projectImage={proyectImage_4} linkGitProject="https://github.com/Velez27/sistemaReportes" title="Proyecto Personal" description="Desarrollo de Control de Tickets en Java">
+          <CarruselItemImage icono={javaIcon} alt={altJava} />
         </CarruselItem>
       </Carrusel>
     </ListaProyectos>
